a7c846cfe15650fc547d9536fb4b039ff05db107
elnurhajiyev <hajiyevelnur92@gmail.com>
2019-07-15 04:24:36 +0200

    fake data library was added, view model was prepared

--- app/src/main/java/com/studydrive/task/MainActivity.kt
+++ app/src/main/java/com/studydrive/task/MainActivity.kt
@@ -2,11 +2,67 @@ package com.studydrive.task
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.RecyclerView
+import com.github.javafaker.Faker
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var mainViewModel : MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainViewModel = ViewModelProviders.of(this,
+            (application as CreateItemApp).viewModelFactory).get(MainViewModel::class.java)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val faker = Faker()
+
+        val producer = findViewById<Button>(R.id.add_producer)
+        val consumer = findViewById<Button>(R.id.add_consumer)
+
+        producer.setOnClickListener {
+            val fullname = faker.name().fullName()
+            val id = UUID.randomUUID().toString()
+            mainViewModel.addItem(
+                CreateItem(
+                    id = id,
+                    text = "Producer: $fullname"
+                )
+            )
+        }
+
+        consumer.setOnClickListener {
+            val fullname = faker.name().fullName()
+            val id = UUID.randomUUID().toString()
+            mainViewModel.addItem(
+                CreateItem(
+                    id = id,
+                    text = "Consumer: $fullname"
+                )
+            )
+        }
+
+        val list = findViewById<RecyclerView>(R.id.list)
+        list.adapter = adapter
+
+        mainViewModel.items.observe(this, Observer { adapter.submitList(it) })
     }
+
+    private val adapter = object : CreateItemListAdapter() {
+        override fun deleteItem(itemId: String) {
+            mainViewModel.deleteById(itemId)
+        }
+    }
+
 }
